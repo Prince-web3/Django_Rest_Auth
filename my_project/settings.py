42895640849957b82015254efdac7b06fc8fc551
EHIMARE0014 <bellacane001@gmail.com>
2025-09-02 14:37:00 +0100

    first commit

--- my_project/settings.py
+++ my_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["rest-api-4uud.onrender.com","localhost", "127.0.0.1"])
 
 
 # Application definition
@@ -146,7 +146,7 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 # CORS
-CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS", default=["http://localhost:3000"])
+CORS_ALLOWED_ORIGINS = env.list("ALLOWED_HOST", default=["http://localhost:3000"])
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
